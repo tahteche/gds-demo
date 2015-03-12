8a8573169259ea6c66c2433099d1fd591c347154
tahteche <tahteche@gmail.com>
2015-03-12 16:59:33 +0100

    no commit

--- config.php
+++
@@ -1,8 +0,0 @@
-<?php
-	
-	$google_api_config = [
-	  'application-id' => 'gds-demo',
-	  'service-account-name' => '1003835728794-dg595ihv2gha7bcprc0nmdhr90j5nh2s@developer.gserviceaccount.com',
-	  'private-key' => file_get_contents('35eaf7a936c2.p12'),
-	  'dataset-id' => 'gds-demo'
-	];
